f105bf0852104847718a48a8cf9b514e110fb760
Micheal Ukeje <ukejemicheal@gmail.com>
2020-07-22 22:39:25 +0100

    Update WordToBinary.py

--- WordToBinary.py
+++ WordToBinary.py
@@ -1,15 +1,19 @@
 """WordToBinary Converter
-This code is up for Optimization,
-your contribution is welcome, coz its a means for me to learn
 
-This code converts user inputed words to its binary equivalence
+This is a simple code which converts user inputed words to its binary equivalence
+
+It shows how methods communicate with each other, i.e.
+some methods can be utility while some carries out the actual computing.
+
+
 First the words are converted into  ASCII using ord()
-then converted to binary which is formatted and printed
+then converted to binary which is formatted and printed.
+
 
 PS: incase you wanted be geekie and tell your girl
 " I Love You" in binary LOL
 
-#Written by: Micheal "BlackRose" Ukeje
+
 
 """
 
@@ -29,8 +33,7 @@ class WordToBinary:
         # Result is passed to be converted to ASCII
         self.ord_converter(self.word_sliced)
 
-        # ASCII converter utility method
-
+    # ASCII converter utility method
     def ord_converter(self, word_sl):
         self.word_ord = []
         # word_length = len(self.word)
